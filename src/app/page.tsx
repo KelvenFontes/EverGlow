3a2344f1b505ec4ce40d9e1c644ae7e2672f325b
Kelven Fontes <kelvenbentofontes@gmail.com>
2023-08-06 11:38:11 -0300

    feat: add new logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,13 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex flex-col bg-primary h-screen">
-      <Image src="/logo.png" width={100} height={100} alt="logo" />
-      <div className="relative w-full h-[407px] top-4">
+      <div className=" flex items-center justify-center p-4">
+        <Image src="/logo.png" width={300} height={100} alt="logo" />
+      </div>
+      <div className="relative w-full h-[407px]">
         <Image src="/img_girl.png" style={{ objectFit: 'cover' }} fill alt="image" />
       </div>
-      <div className="flex flex-col fixed bottom-0 text-center rounded-t-[50px] bg-black/95 pt-10 pl-6 pr-6 pb-20 gap-10 h-80">
+      <div className="flex flex-col fixed bottom-0 text-center rounded-t-[50px] bg-black/95 pt-10 pl-6 pr-6 pb-10 gap-10 h-80">
         <p className="text-white font-semibold text-2xl">
           From the <span className="text-primary">latest</span> to the
           <span className="text-primary"> greatest</span> hits,
