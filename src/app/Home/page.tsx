1e90acd764dae119141a504638bffcbc3d6ddbb5
Kelven Fontes <kelvenbentofontes@gmail.com>
2023-08-06 21:58:38 -0300

    feat: add component structure Footer

--- src/app/Home/page.tsx
+++ src/app/Home/page.tsx
@@ -1,9 +1,15 @@
+import Footer from "@/components/Footer";
+import ContinueListening from "./components/ContinueListening";
 import Header from "./components/Header";
+import TopMixes from "./components/TopMixes";
 
 const Home = () => {
   return (
     <div className="bg-dark h-screen">
       <Header />
+      <ContinueListening />
+      <TopMixes />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,22 @@
+import { BiBookBookmark, BiHomeAlt2, BiSearchAlt2 } from 'react-icons/bi';
+
+const Footer = () => {
+  return (
+    <div className="flex items-center justify-around bg-black py-4">
+      <div className='flex flex-col items-center justify-center gap-1'>
+        <BiHomeAlt2 className='text-primary' size={20}/>
+        <span className='text-primary text-sm font-semibold'>Home</span>
+      </div>
+      <div className='flex flex-col items-center justify-center gap-1'>
+        <BiSearchAlt2 className='text-white' size={20}/>
+        <span className='text-white text-sm font-semibold'>Explore</span>
+      </div>
+      <div className='flex flex-col items-center justify-center gap-1'>
+        <BiBookBookmark className='text-white' size={20}/>
+        <span className='text-white text-sm font-semibold'>Library</span>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
