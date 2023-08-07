aac7878c585a006186daaf21744def2468e77f6b
Kelven Fontes <kelvenbentofontes@gmail.com>
2023-08-06 21:57:56 -0300

    feat: add component structure ContinueListening

---
+++ src/app/Home/components/ContinueListening.tsx
@@ -0,0 +1,30 @@
+const ContinueListening = () => {
+  return (
+    <div>
+      <h2>Continue Listening</h2>
+      <div className="grid grid-cols-2">
+        <div>
+          <h1 className="text-white">coffee & jazz</h1>
+        </div>
+        <div>
+          <h1 className="text-white">coffee & jazz</h1>
+        </div>
+        <div>
+          <h1 className="text-white">coffee & jazz</h1>
+        </div>
+        <div>
+          <h1 className="text-white">coffee & jazz</h1>
+        </div>
+        <div>
+          <h1 className="text-white">coffee & jazz</h1>
+        </div>
+         <div>
+          <h1 className="text-white">coffee & jazz</h1>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
+
+export default ContinueListening;
