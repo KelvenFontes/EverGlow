497dcd40febb6988b9fc3d52d0b09f55b1c5f03f
Kelven Fontes <kelvenbentofontes@gmail.com>
2023-09-01 21:47:30 -0300

    style: Adjuste Space

--- src/app/Home/components/CardContinueListening.tsx
+++ src/app/Home/components/CardContinueListening.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const CardContinueListening = () => {
   return (
-    <div className="flex items-center gap-4 bg-darkLight h-12 rounded-md">
+    <div className="flex items-center gap-2 bg-darkLight h-12 rounded-md">
       <div className="relative h-12 w-12">
         <Image src="/logo.png" alt="logo" className="rounded-l-md" style={{ objectFit: 'cover' }} fill />
       </div>
