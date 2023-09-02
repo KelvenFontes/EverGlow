b8af7c31bbd600d3fc2aa6441db2224e1983c00e
Kelven Fontes <kelvenbentofontes@gmail.com>
2023-09-01 23:47:21 -0300

    feat: add Library

---
+++ src/app/Library/components/Header.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+
+const Header = () => {
+  return (
+    <div className="pt-8 items-center">
+      <div className="col-span-2">
+        <div className="flex flex-row items-center gap-6 pl-5">
+          <Image src='/icon.png' alt="image" height={40} width={40} className="rounded-full" />
+          <div>
+          <p className="text-primary font-semibold text-2xl tracking-widest">Your Library</p>
+
+          </div>
+        </div>
+      </div>
+
+      <div className="flex items-center justify-center pt-6">
+        <input type="text" className="h-10 w-[90%] rounded-2xl pl-6" placeholder="Songs, Artists, Podcasts & More"/>
+      </div>
+
+    </div>
+  );
+}
+
+export default Header;
