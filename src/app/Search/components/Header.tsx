8a89f2488fca1d20c32989ee9c287d92d1bffc83
Kelven Fontes <kelvenbentofontes@gmail.com>
2023-09-02 18:08:58 -0300

    feat: add search

--- src/app/Search/components/Header.tsx
+++ src/app/Search/components/Header.tsx
@@ -1,21 +1,109 @@
+import Button from "@/components/Button";
 import Image from "next/image";
+// import { SetStateAction, useState } from "react";
 
-const Header = () => {
+import { FaMusic, FaPodcast, FaArtstation, FaSearch } from "react-icons/fa";
+
+interface HeaderProps {
+  search: string | undefined;
+  setSearch: (value: string) => void;
+  selectedCategory: string | undefined;
+  setSelectedCategory: (value: string) => void;
+  getSearch: () => void;
+}
+
+const Header = ({ search, setSearch, selectedCategory, setSelectedCategory, getSearch }: HeaderProps) => {
+
+  const handleSearchChange = (e: any) => {
+    setSearch(e.target.value);
+  };
+
+  const handleCategorySelect = (category: string) => {
+    setSelectedCategory(category);
+  };
+
+  const handleSearch = () => {
+    getSearch();
+  };
+
   return (
     <div className="pt-8 items-center">
       <div className="col-span-2">
         <div className="flex flex-row items-center gap-6 pl-5">
           <Image src='/icon.png' alt="image" height={40} width={40} className="rounded-full" />
           <div>
-          <h2 className="text-primary font-semibold text-2xl tracking-widest">Search</h2>
+            <h2 className="text-primary font-semibold text-2xl tracking-widest">Search</h2>
 
           </div>
         </div>
       </div>
 
-      <div className="flex items-center justify-center pt-6">
-        <input type="text" className="h-10 w-[90%] rounded-2xl pl-6" placeholder="Songs, Artists, Podcasts & More"/>
+      <div className="flex items-center justify-center pt-6 gap-3">
+        <input type="text" className="h-10 w-[75%] rounded-2xl pl-4" placeholder="Songs, Artists, Podcasts & More" value={search} onChange={handleSearchChange} />
+        <Button onClick={handleSearch}><FaSearch /></Button>
       </div>
+
+      <div className="flex items-center justify-center pt-4">
+        <div
+          className={`cursor-pointer mx-2 ${selectedCategory === "album" ? "text-primary" : "text-white"
+            }`}
+          onClick={() => handleCategorySelect("album")}
+        >
+          <FaMusic size={24} />
+          <span className="text-xs">Album</span>
+        </div>
+        <div
+          className={`cursor-pointer mx-2 ${selectedCategory === "artist" ? "text-primary" : "text-white"
+            }`}
+          onClick={() => handleCategorySelect("artist")}
+        >
+          <FaPodcast size={24} />
+          <span className="text-xs">Artist</span>
+        </div>
+        <div
+          className={`cursor-pointer mx-2 ${selectedCategory === "playlist" ? "text-primary" : "text-white"
+            }`}
+          onClick={() => handleCategorySelect("playlist")}
+        >
+          <FaArtstation size={24} />
+          <span className="text-xs">Playlist</span>
+        </div>
+
+        <div
+          className={`cursor-pointer mx-2 ${selectedCategory === "track" ? "text-primary" : "text-white"
+            }`}
+          onClick={() => handleCategorySelect("track")}
+        >
+          <FaArtstation size={24} />
+          <span className="text-xs">Track</span>
+        </div>
+        <div
+          className={`cursor-pointer mx-2 ${selectedCategory === "show" ? "text-primary" : "text-white"
+            }`}
+          onClick={() => handleCategorySelect("show")}
+        >
+          <FaArtstation size={24} />
+          <span className="text-xs">Show</span>
+        </div>
+        <div
+          className={`cursor-pointer mx-2 ${selectedCategory === "episode" ? "text-primary" : "text-white"
+            }`}
+          onClick={() => handleCategorySelect("episode")}
+        >
+          <FaArtstation size={24} />
+          <span className="text-xs">Episode</span>
+        </div>
+        <div
+          className={`cursor-pointer mx-2 ${selectedCategory === "audioBook" ? "text-primary" : "text-white"
+            }`}
+          onClick={() => handleCategorySelect("audioBook")}
+        >
+          <FaArtstation size={24} />
+          <span className="text-xs">AudioBook</span>
+        </div>
+
+      </div>
+
 
     </div>
   );
--- src/app/Search/page.tsx
+++ src/app/Search/page.tsx
@@ -1,13 +1,63 @@
+'use client'
+
 import Footer from "@/components/Footer";
 import Header from "./components/Header";
 import TopGenres from "./components/TopGenres";
 import BrowseAll from "./components/BrowseAll";
+import { useEffect, useState } from "react";
 
 const Search = () => {
+
+  const [search, setSearch] = useState<string | undefined>();
+  const [selectedCategory, setSelectedCategory] = useState("");
+
+  const access_token = localStorage.getItem('access_token');
+
+  if (search != '') {
+    getSearch()
+  }
+
+  async function getSearch() {
+
+    const params = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + access_token
+      },
+    };
+
+    if (selectedCategory) {
+      console.log(selectedCategory);
+
+      const result = await fetch(`https://api.spotify.com/v1/search?q=${search}remaster&type=${selectedCategory}`, params);
+      const data = await result.json();
+
+      console.log(data);
+
+      if (data.categories) {
+        setSearch(data.categories.items);
+      }
+    } else {
+      console.log('selecione uma categoria')
+    }
+
+  }
+
+  useEffect(() => {
+    // if (access_token) {
+    //   console.log('Access Token:', access_token);
+    //   getSearch(access_token);
+    // } else {
+    //   console.log('Access Token não encontrado no localStorage.');
+    // }
+
+  }, []);
+
   return (
     <div className="flex flex-col min-h-screen bg-dark">
       <div className="bg-gradient-to-b from-primaryLight to-dark">
-        <Header />
+        <Header search={search} setSearch={setSearch} selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} getSearch={getSearch}/>
       </div>
 
       <div className="flex-1 pb-24">
