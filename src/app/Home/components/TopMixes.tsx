0ae9480e6112c7f75f18b02454d598eeb7cf14d6
Kelven Fontes <kelvenbentofontes@gmail.com>
2023-09-01 22:28:58 -0300

    feat: add scroll Top Mix

--- src/app/Home/components/TopMixes.tsx
+++ src/app/Home/components/TopMixes.tsx
@@ -1,13 +1,110 @@
+'use client'
+
+import { useState } from "react";
 import CardTopMix from "./CardTopMix";
+import Link from "next/link";
 
 const TopMixes = () => {
+
   return (
     <div className="container mx-auto pl-5 pt-8">
-
       <h2 className="text-white text-lg font-semibold pb-3">Your Top Mixes</h2>
-      <CardTopMix />
-      {/* <CardTopMix />
-      <CardTopMix /> */}
+      <div className="container mx-auto mt-5 lg:mt-10">
+        <div className="relative flex items-center pt-3 lg:justify-center">
+          <div className="w-full overflow-x-hidden scroll whitespace-nowrap scroll-smooth scrollbar-hide lg:flex lg:items-center lg:justify-center">
+            {/* Primeiro Slide */}
+            <div className="inline-block cursor-pointer pr-6 transform transition-transform hover:scale-105">
+              <Link href={`/products/search/`}>
+                <div className="text-orange-400 flex flex-col items-center justify-center lg:mr-10">
+                  <CardTopMix />
+                </div>
+              </Link>
+            </div>
+
+            {/* Segundo Slide */}
+            <div className="inline-block cursor-pointer pr-6 transform transition-transform hover:scale-105">
+              <Link href={`/products/search/`}>
+                <div className="text-orange-400 flex flex-col items-center justify-center lg:mr-10">
+                  <CardTopMix />
+                </div>
+              </Link>
+            </div>
+
+            <div className="inline-block cursor-pointer pr-6 transform transition-transform hover:scale-105">
+              <Link href={`/products/search/`}>
+                <div className="text-orange-400 flex flex-col items-center justify-center lg:mr-10">
+                  <CardTopMix />
+                </div>
+              </Link>
+            </div>
+
+            <div className="inline-block cursor-pointer pr-6 transform transition-transform hover:scale-105">
+              <Link href={`/products/search/`}>
+                <div className="text-orange-400 flex flex-col items-center justify-center lg:mr-10">
+                  <CardTopMix />
+                </div>
+              </Link>
+            </div>
+
+            <div className="inline-block cursor-pointer pr-6 transform transition-transform hover:scale-105">
+              <Link href={`/products/search/`}>
+                <div className="text-orange-400 flex flex-col items-center justify-center lg:mr-10">
+                  <CardTopMix />
+                </div>
+              </Link>
+            </div>
+
+            <div className="inline-block cursor-pointer pr-6 transform transition-transform hover:scale-105">
+              <Link href={`/products/search/`}>
+                <div className="text-orange-400 flex flex-col items-center justify-center lg:mr-10">
+                  <CardTopMix />
+                </div>
+              </Link>
+            </div>
+
+            <div className="inline-block cursor-pointer pr-6 transform transition-transform hover:scale-105">
+              <Link href={`/products/search/`}>
+                <div className="text-orange-400 flex flex-col items-center justify-center lg:mr-10">
+                  <CardTopMix />
+                </div>
+              </Link>
+            </div>
+
+            <div className="inline-block cursor-pointer pr-6 transform transition-transform hover:scale-105">
+              <Link href={`/products/search/`}>
+                <div className="text-orange-400 flex flex-col items-center justify-center lg:mr-10">
+                  <CardTopMix />
+                </div>
+              </Link>
+            </div>
+
+            <div className="inline-block cursor-pointer pr-6 transform transition-transform hover:scale-105">
+              <Link href={`/products/search/`}>
+                <div className="text-orange-400 flex flex-col items-center justify-center lg:mr-10">
+                  <CardTopMix />
+                </div>
+              </Link>
+            </div>
+
+            <div className="inline-block cursor-pointer pr-6 transform transition-transform hover:scale-105">
+              <Link href={`/products/search/`}>
+                <div className="text-orange-400 flex flex-col items-center justify-center lg:mr-10">
+                  <CardTopMix />
+                </div>
+              </Link>
+            </div>
+
+            <div className="inline-block cursor-pointer pr-6 transform transition-transform hover:scale-105">
+              <Link href={`/products/search/`}>
+                <div className="text-orange-400 flex flex-col items-center justify-center lg:mr-10">
+                  <CardTopMix />
+                </div>
+              </Link>
+            </div>
+
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
