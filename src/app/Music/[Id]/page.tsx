82da886c5943c5e8d23453af6b17c48b13c6b17b
Kelven Fontes <kelvenbentofontes@gmail.com>
2023-09-03 17:22:14 -0300

    feat: add progressBar

--- src/app/Music/[Id]/page.tsx
+++ src/app/Music/[Id]/page.tsx
@@ -9,7 +9,9 @@ const MusicById = ({ params }: { params: { Id: string } }) => {
 
   const [token, setToken] = useState<string>('');
   const [device, setDevice] = useState([]);
+  const [musicPlaying, setMusicPlaying] = useState<any[] | any | null | undefined>([]);
   const musicId = params.Id;
+  const [musicProgress, setMusicProgress] = useState<number | undefined>(0);
 
   useEffect(() => {
 
@@ -19,40 +21,64 @@ const MusicById = ({ params }: { params: { Id: string } }) => {
       setToken(autenticator_token!);
       playMusic(autenticator_token!, musicId);
       getDevice(autenticator_token!);
-      playMusicA(autenticator_token!);
-      console.log(autenticator_token);
+      getMusicPlaying(autenticator_token!);
+
+      const intervalId = setInterval(() => {
+        getMusicPlaying(autenticator_token!);
+      }, 1000); // Atualiza a cada segundo (você pode ajustar o intervalo conforme necessário)
+
+      // Limpa o intervalo quando o componente é desmontado
+      return () => clearInterval(intervalId);
     } else {
       setTimeout(() => {
         playMusic(token, musicId);
         getDevice(token);
+        getMusicPlaying(token);
       }, 5000)
     }
 
-    // getArtist(token, musicId);
 
-  }, [musicId, token]);
 
-  async function getDevice(token: String) {
-    console.log('entrou no device')
+  }, []);
+
+  async function getMusicPlaying(token: string) {
     const params = {
       headers: {
         'Authorization': 'Bearer ' + token,
         'Content-Type': 'application/json'
       },
     };
-
-    const result = await fetch(`https://api.spotify.com/v1/me/player/devices`, params);
+    const result = await fetch(`https://api.spotify.com/v1/me/player/currently-playing`, params);
     const data = await result.json();
 
-    console.log(result);
+    if (data && data.item) {
+      // Acesse progress_ms e duration_ms dentro de data.item
+      const progressMs = data.progress_ms;
+      const durationMs = data.item.duration_ms;
 
-    // setArtist(data.tracks);
-    console.log(data);
-    setDevice(data.device);
-    console.log(device);
-
+      setMusicPlaying(data.item);
+      setMusicProgress(progressMs);
+      console.log(data);
+      console.log(progressMs);
+    } else {
+      // Lide com o caso em que não há música em reprodução no momento
+      console.log("Nenhuma música em reprodução no momento.");
+    }
   }
 
+
+
+  async function getDevice(token: String) {
+    const params = {
+      headers: {
+        'Authorization': 'Bearer ' + token,
+        'Content-Type': 'application/json'
+      },
+    };
+    const result = await fetch(`https://api.spotify.com/v1/me/player/devices`, params);
+    const data = await result.json();
+    setDevice(data.device);
+  }
 
   async function playMusic(token: string, musicId: string) {
     const params = {
@@ -67,55 +93,48 @@ const MusicById = ({ params }: { params: { Id: string } }) => {
       })
     };
     const result = await fetch(`https://api.spotify.com/v1/me/player/play`, params);
-
   }
-
-  async function playMusicA(token: string) {
-    const params = {
-      // method: 'PUT',
-      headers: {
-        'Authorization': 'Bearer ' + token,
-        'Content-Type': 'application/json'
-      },
-      // body: JSON.stringify({
-      //   "uris": [`spotify:track:${musicId}`],
-      //   "device_id": "14f5302560cfebe1b8e7a425b6d7894a4708f15c"
-      // })
-    };
-    const result = await fetch(`https://api.spotify.com/v1/me/player/currently-playing`, params);
-    const data = await result.json();
-    console.log(result);
-    console.log(data);
-  }
-
-
 
   return (
     <div className="container mx-auto min-h-screen flex flex-col bg-dark">
-      <div className="bg-gradient-to-b from-primaryLight to-dark">
+      <div className="bg-gradient-to-b from-primaryLight to-dark" >
         <Header />
-      </div>
+      </div >
 
-      <div className="flex flex-col items-center justify-center">
-        {/* <p className="text-white">teste</p> */}
-        <Image src="/Music/Rectangle 19.png" alt="logo" height={252} width={263} className="mt-20" />
+      {musicPlaying && musicPlaying.album && musicPlaying.album.images && musicPlaying.album.images.length > 0 ? (
+        <div className="flex flex-col items-center justify-center">
+          <Image src={musicPlaying.album.images[0].url} alt={musicPlaying.name} height={252} width={263} className="mt-20" />
+          <div className="ml-4">
+            <h3 className="text-lg text-gray-300 font-medium">{musicPlaying.name}</h3>
+            <h3 className="text-lg text-gray-300 font-medium">{musicPlaying.album.name}</h3>
+          </div>
 
-        <h3>Grainly days</h3>
-        <p>moody</p>
+          {/* Barra de Progresso */}
+          <div className="bg-gray-100 w-[80%] h-2 rounded-full">
+            <div className="bg-primary h-full rounded-full" style={{ width: `${(musicProgress! / musicPlaying.duration_ms) * 100}%` }}></div>
+          </div>
 
-        {/*lista*/}
-        <div></div>
-        {/*lista*/}
+          <div></div>
+
+          <div>
+            <div>player</div>
+            <div>pause</div>
+
+            <div></div>
+          </div>
 
 
-        {/*player*/}
-        <div></div>
-        {/*player*/}
+        </div>
+      ) : (
+        <div className="flex items-center">
+          <Image src='/icon.png' alt="Default Image" height={80} width={80} className="object-cover rounded-xl" />
+          <div className="ml-4">
+            <h3 className="text-lg text-gray-300 font-medium">No music playing</h3>
+          </div>
+        </div>
+      )}
 
-
-      </div>
-
-    </div>
+    </div >
 
   );
 }
--- src/components/FooterMusic.tsx
+++ src/components/FooterMusic.tsx
@@ -2,50 +2,84 @@
 
 import { useEffect, useState } from "react";
 import Image from 'next/image';
+import Link from "next/link";
 
 const FooterMusic = () => {
 
-  // const [token, setToken] = useState<string>('');
+  const [token, setToken] = useState<string>('');
   const [musicPlaying, setMusicPlaying] = useState<any[] | any | undefined | null>([]);
+
+  const [musicProgress, setMusicProgress] = useState<number | undefined>(0);
+
 
   useEffect(() => {
 
     const autenticator_token = localStorage.getItem('access_token');
+    setToken(autenticator_token!)
+
 
     if (autenticator_token != '') {
-      setTimeout(() => {
-        getMusicPlaying(autenticator_token!);
-      }, 2000)
-    }
+      getMusicPlaying(autenticator_token!);
 
-    async function getMusicPlaying(token: string) {
-      const params = {
-        headers: {
-          'Authorization': 'Bearer ' + token,
-          'Content-Type': 'application/json'
-        },
-      };
-      const result = await fetch(`https://api.spotify.com/v1/me/player/currently-playing`, params);
-      const data = await result.json();
-      setMusicPlaying(data.item);
-      console.log(result);
-      console.log(data);
+      const intervalId = setInterval(() => {
+        getMusicPlaying(autenticator_token!);
+      }, 1000); // Atualiza a cada segundo (você pode ajustar o intervalo conforme necessário)
+
+      // Limpa o intervalo quando o componente é desmontado
+      return () => clearInterval(intervalId);
+
     }
 
   }, []);
+
+  async function getMusicPlaying(token: string) {
+    const params = {
+      headers: {
+        'Authorization': 'Bearer ' + token,
+        'Content-Type': 'application/json'
+      },
+    };
+    const result = await fetch(`https://api.spotify.com/v1/me/player/currently-playing`, params);
+    const data = await result.json();
+
+    if (data && data.item) {
+      // Acesse progress_ms e duration_ms dentro de data.item
+      const progressMs = data.progress_ms;
+      const durationMs = data.item.duration_ms;
+
+      setMusicPlaying(data.item);
+      setMusicProgress(progressMs);
+      console.log(data);
+      console.log(progressMs);
+    } else {
+      // Lide com o caso em que não há música em reprodução no momento
+      console.log("Nenhuma música em reprodução no momento.");
+    }
+  }
 
 
 
   console.log(musicPlaying);
   return (
     <div className="flex items-center justify-around py-4 pb-5 fixed bottom-20 left-0 right-0 h-20 bg-gradient-to-b from-primary to-dark">
+
       {musicPlaying && musicPlaying.album && musicPlaying.album.images && musicPlaying.album.images.length > 0 ? (
-        <div className="flex items-center">
-          <Image src={musicPlaying.album.images[0].url} alt={musicPlaying.name} height={80} width={80} className="object-cover rounded-xl" />
-          <div className="ml-4">
-            <h3 className="text-lg text-gray-300 font-medium">{musicPlaying.name}</h3>
+        <Link href={`/Music/${musicPlaying.id}`}>
+          <div className="w-[100%]">
+            <div className="flex items-center">
+              <Image src={musicPlaying.album.images[0].url} alt={musicPlaying.name} height={80} width={80} className="object-cover rounded-xl" />
+              <div className="ml-4">
+                <h3 className="text-lg text-gray-300 font-medium">{musicPlaying.name}</h3>
+              </div>
+            </div>
+
+            {/* Barra de Progresso */}
+            <div className="bg-gray-100 w-[80%] h-2 rounded-full">
+              <div className="bg-primary h-full rounded-full" style={{ width: `${(musicProgress! / musicPlaying.duration_ms) * 100}%` }}></div>
+            </div>
+
           </div>
-        </div>
+        </Link>
       ) : (
         <div className="flex items-center">
           <Image src='/icon.png' alt="Default Image" height={80} width={80} className="object-cover rounded-xl" />
@@ -53,9 +87,8 @@ const FooterMusic = () => {
             <h3 className="text-lg text-gray-300 font-medium">No music playing</h3>
           </div>
         </div>
-      )}
-
-
+      )
+      }
     </div >
 
   );
